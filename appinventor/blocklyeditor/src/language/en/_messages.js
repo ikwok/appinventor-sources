db2c41f5400174536747f025f3f9c98ca139f139
Irene Kwok <ikwok@wellesley.edu>
2013-09-28 15:44:40 -0400

    added messages

--- appinventor/blocklyeditor/src/language/en/_messages.js
+++ appinventor/blocklyeditor/src/language/en/_messages.js
@@ -616,6 +616,16 @@ Blockly.LANG_LISTS_PICK_RANDOM_TITLE_PICK_RANDOM = 'pick a random item';
 Blockly.LANG_LISTS_PICK_RANDOM_ITEM_INPUT_LIST = 'list';
 Blockly.LANG_LISTS_PICK_RANDOM_TOOLTIP = 'Pick an item at random from the list.';
 
+Blockly.LANG_LISTS_PICK_FIRST_ITEM_HELPURL = 'http://appinventor.mit.edu/explore/ai2/support/blocks/lists#pickfirstitem';
+Blockly.LANG_LISTS_PICK_FIRST_TITLE_PICK_FIRST = 'pick the first item';
+Blockly.LANG_LISTS_PICK_FIRST_ITEM_INPUT_LIST = 'list';
+Blockly.LANG_LISTS_PICK_FIRST_TOOLTIP = 'Pick the first item from the list.';
+
+Blockly.LANG_LISTS_PICK_LAST_ITEM_HELPURL = 'http://appinventor.mit.edu/explore/ai2/support/blocks/lists#picklastitem';
+Blockly.LANG_LISTS_PICK_LAST_TITLE_PICK_RANDOM = 'pick the last item';
+Blockly.LANG_LISTS_PICK_LAST_ITEM_INPUT_LIST = 'list';
+Blockly.LANG_LISTS_PICK_LAST_TOOLTIP = 'Pick the last item from the list.';
+
 Blockly.LANG_LISTS_REPLACE_ITEM_HELPURL = 'http://appinventor.mit.edu/explore/ai2/support/blocks/lists#replace';
 Blockly.LANG_LISTS_REPLACE_ITEM_TITLE_REPLACE = 'replace list item';
 Blockly.LANG_LISTS_REPLACE_ITEM_INPUT_LIST = 'list';
